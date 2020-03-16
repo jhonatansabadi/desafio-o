0cde5b3149312fc5a1ffe39e622bb3abf0d45a7f
jhonatan.sabadi <jhonatan.sabadi@icnavais.com>
2020-03-16 10:09:56 -0300

    Configuração de RecyclerView

--- app/src/main/java/jhonatan/sabadi/desafioo/ui/activity/MainActivity.java
+++ app/src/main/java/jhonatan/sabadi/desafioo/ui/activity/MainActivity.java
@@ -1,15 +1,25 @@
 package jhonatan.sabadi.desafioo.ui.activity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import jhonatan.sabadi.desafioo.R;
+import jhonatan.sabadi.desafioo.interfaces.OnRecyclerClickListener;
+import jhonatan.sabadi.desafioo.model.Fund;
+import jhonatan.sabadi.desafioo.ui.adapter.FundAdapter;
 import jhonatan.sabadi.desafioo.ui.viewmodel.FundViewModel;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnRecyclerClickListener {
 
     private FundViewModel fundViewModel = ViewModelProviders.of(this).get(FundViewModel.class);
+    private FundAdapter fundAdapter;
+    private RecyclerView fundRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +30,24 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void initRecyclerView(List<Fund> funds) {
+        fundRecyclerView = findViewById(R.id.fundRecyclerView);
+        fundRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        fundRecyclerView.setHasFixedSize(true);
+        fundAdapter = new FundAdapter(funds, this);
+        fundRecyclerView.setAdapter(fundAdapter);
+    }
+
     private void loadFunds() {
         fundViewModel.getFunds().observe(this, funds -> {
-
+            if (funds != null) {
+                initRecyclerView(funds);
+            }
         });
     }
 
+    @Override
+    public void setOnRecyclerClick(View view, int position) {
+
+    }
 }
