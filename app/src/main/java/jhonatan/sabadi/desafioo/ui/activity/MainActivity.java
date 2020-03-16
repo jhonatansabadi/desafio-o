c35773186b450804624bb24e6c63505392d06c72
jhonatan.sabadi <jhonatan.sabadi@icnavais.com>
2020-03-16 09:49:48 -0300

    Criação de ViewModel

--- app/src/main/java/jhonatan/sabadi/desafioo/ui/activity/MainActivity.java
+++ app/src/main/java/jhonatan/sabadi/desafioo/ui/activity/MainActivity.java
@@ -1,15 +1,34 @@
 package jhonatan.sabadi.desafioo.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import jhonatan.sabadi.desafioo.R;
+import jhonatan.sabadi.desafioo.model.Fund;
+import jhonatan.sabadi.desafioo.ui.viewmodel.FundViewModel;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FundViewModel fundViewModel = ViewModelProviders.of(this).get(FundViewModel.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFunds();
+
     }
+
+    private void loadFunds() {
+        fundViewModel.getFunds().observe(this, funds -> {
+
+        });
+    }
+
 }
---
+++ app/src/main/java/jhonatan/sabadi/desafioo/ui/viewmodel/FundViewModel.java
@@ -0,0 +1,26 @@
+package jhonatan.sabadi.desafioo.ui.viewmodel;
+
+import android.app.Application;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import jhonatan.sabadi.desafioo.controller.FundController;
+import jhonatan.sabadi.desafioo.model.Fund;
+
+public class FundViewModel extends AndroidViewModel {
+
+    private FundController fundController;
+
+    public FundViewModel(@NonNull Application application) {
+        super(application);
+        this.fundController = new FundController(application);
+    }
+
+    public LiveData<List<Fund>> getFunds() {
+        return fundController.getFunds();
+    }
+
+}
