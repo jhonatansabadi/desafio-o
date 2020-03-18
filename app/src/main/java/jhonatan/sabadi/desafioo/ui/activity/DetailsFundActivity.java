2cd26538b4ce91d4789501c8abd84d87431e797a
jhonatansabadi <jhonatansabadi@hotmail.com>
2020-03-18 08:55:36 -0300

    Adicionando scroll layout no activity detail

--- app/src/main/java/jhonatan/sabadi/desafioo/ui/activity/DetailsFundActivity.java
+++ app/src/main/java/jhonatan/sabadi/desafioo/ui/activity/DetailsFundActivity.java
@@ -2,6 +2,10 @@ package jhonatan.sabadi.desafioo.ui.activity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import androidx.appcompat.app.AppCompatActivity;
 import jhonatan.sabadi.desafioo.R;
@@ -9,14 +13,22 @@ import jhonatan.sabadi.desafioo.model.Fund;
 
 public class DetailsFundActivity extends AppCompatActivity {
 
+    private ImageView image_view_details;
+    private TextView full_name_details;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_fund);
 
+        loadComponents();
         loadFundFromIntent();
 
+    }
+
+    private void loadComponents() {
+        image_view_details = findViewById(R.id.image_view_details);
+        full_name_details = findViewById(R.id.full_name_details);
     }
 
     private void loadFundFromIntent() {
@@ -28,6 +40,15 @@ public class DetailsFundActivity extends AppCompatActivity {
     }
 
     private void setDetailsToText(Fund fund) {
+        setImageLogo(fund);
+        full_name_details.setText(fund.getFull_name());
+    }
+
+    private void setImageLogo(Fund fund) {
+        String logoUrl = getString(R.string.base_url_logo) + fund.getFund_manager().getLogo();
+        Glide.with(this)
+                .load(logoUrl)
+                .into(image_view_details);
 
     }
 
